306284b2b6203e54d1a0487a6c51f9dfcc79636f
SealabJaster <sealabjaster@gmail.com>
2018-03-19 17:07:42 +0000

    Restructure how the serialisation generation code is layed out

--- source/jarena/data/serialise.d
+++ source/jarena/data/serialise.d
@@ -5,6 +5,10 @@ private
     import std.traits;
     import sdlang;
     import jarena.data.loaders, jarena.core;
+
+    import std.format : format;
+    import std.conv   : to;
+    import codebuilder;
 }
 
 /++
@@ -61,17 +65,9 @@ mixin template SerialisableInterface()
 // But shouldn't be used outside of this module.
 string fromSdlTagGenerator(ThisType)()
 {
-    import std.format : format;
-    import std.conv   : to;
-    import codebuilder;
     auto code = new CodeBuilder();
 
     size_t nameCounter = 0;
-    string genTempName()
-    {
-        import std.conv : to;
-        return "var" ~ nameCounter++.to!string;
-    }
 
     foreach(fieldName; FieldNameTuple!ThisType)
     {
@@ -80,45 +76,11 @@ string fromSdlTagGenerator(ThisType)()
         enum  FieldTypeName = fullyQualifiedName!FieldType;
         enum  FieldTagName  = getFieldName!FieldAlias.name;        
 
-        // For builtin types, use expectTagValue, since it already supports them all.
-        static if(isBuiltinType!FieldType)
-        {
-            code.putf("this.%s = tag.expectTagValue!(%s)(\"%s\");",
-                      fieldName, FieldTypeName, FieldTagName);
-        }
-        else static if(is(FieldType == struct) && hasUDA!(FieldType, Serialisable))
-        {
-            // For @Serialisable structs, call their fromSdlTag function.
-            static assert(hasMember!(FieldType, "fromSdlTag"),
-                          format("The @Serialisable type '%s' doesn't have a function called 'fromSdlTag', please use `mixin SerialisableInterface;`",
-                                 FieldType.stringof)
-                         );
-                         
-            code.putf("this.%s = (%s).init;",
-                      fieldName, FieldTypeName);
-
-            code.putf("this.%s.fromSdlTag(tag.expectTag(\"%s\"));",
-                      fieldName, FieldTagName);
-        }
-        else static if(isVector!FieldType)
-        {
-            // For vectors, check that the tag has N amount of values, and then read them into the vector.
-            enum N = FieldType.dimension;
-            alias VectT = Signed!(FieldType.valueType);
-            auto varName = genTempName();
-            code.putf("Tag %s = tag.expectTag(\"%s\");", varName, FieldTagName);
-            code.putf("enforce(%s.values.length == %s, \"Expected %s values for tag %s for type '%s'\");",
-                      varName, N, N, FieldTagName, FieldTypeName);
-                      
-            code.putf("foreach(i; 0..%s)", N);
-            code.putScope((_)
-            {
-                code.putf("this.%s.data[i] = %s.values[i].get!%s;",
-                          fieldName, varName, VectT.stringof);
-            });
-        }
+        
+        static if(is(typeof({code.generateSDL!(FieldAlias, FieldType, FieldTypeName, FieldTagName, fieldName)(nameCounter);})))
+            code.generateSDL!(FieldAlias, FieldType, FieldTypeName, FieldTagName, fieldName)(nameCounter);
         else
-            static assert(false, format("Don't know how to serialise member '%s' of type '%s'", fieldName, FieldType.stringof));
+            static assert(false, format("No Seraliser for field '%s' of type '%s'", fieldName, FieldType.stringof));
     }        
     
     return code.data.idup.to!string;
@@ -130,4 +92,59 @@ private static Name getFieldName(alias F)()
         return getUDAs!(F, Name)[0];
     else
         return Name(F.stringof);
+}
+
+private string genTempName(ref size_t nameCounter)
+{
+    import std.conv : to;
+    return "var" ~ nameCounter++.to!string;
+}
+
+// SDLang specific functions
+// Anytime a new type needs to have a generator for it to be serialised, just make
+// a new 'generateSDL' function, and change it's contract.
+private static
+{
+    // For builtin types, use expectTagValue, since it already supports them all.
+    void generateSDL(alias FieldAlias, FieldType, string FieldTypeName, string FieldTagName, string FieldMemberName)
+                    (CodeBuilder code, ref size_t nameCounter)
+    if(isBuiltinType!FieldType)
+    {
+        code.putf("this.%s = tag.expectTagValue!(%s)(\"%s\");",
+                  FieldMemberName, FieldTypeName, FieldTagName);
+    }
+
+    // For @Serialisable structs, call their fromSdlTag function.
+    void generateSDL(alias FieldAlias, FieldType, string FieldTypeName, string FieldTagName, string FieldMemberName)
+                    (CodeBuilder code, ref size_t nameCounter)
+    if(is(FieldType == struct) && hasUDA!(FieldType, Serialisable))
+    {        
+        static assert(hasMember!(FieldType, "fromSdlTag"),
+                      format("The @Serialisable type '%s' doesn't have a function called 'fromSdlTag', please use `mixin SerialisableInterface;`",
+                             FieldType.stringof)
+                     );
+                         
+        code.putf("this.%s = (%s).init;", FieldMemberName, FieldTypeName);
+        code.putf("this.%s.fromSdlTag(tag.expectTag(\"%s\"));",TagName);
+    }
+
+    // For vectors, check that the tag has N amount of values, and then read them into the vector.
+    void generateSDL(alias FieldAlias, FieldType, string FieldTypeName, string FieldTagName, string FieldMemberName)
+                    (CodeBuilder code, ref size_t nameCounter)
+    if(isVector!FieldType)
+    {
+        enum N = FieldType.dimension;
+        alias VectT = Signed!(FieldType.valueType);
+        auto varName = genTempName(nameCounter);
+        code.putf("Tag %s = tag.expectTag(\"%s\");", varName, FieldTagName);
+        code.putf("enforce(%s.values.length == %s, \"Expected %s values for tag %s for type '%s'\");",
+                  varName, N, N, FieldTagName, FieldTypeName);
+                      
+        code.putf("foreach(i; 0..%s)", N);
+        code.putScope((_)
+        {
+            code.putf("this.%s.data[i] = %s.values[i].get!%s;",
+                      FieldMemberName, varName, VectT.stringof);
+        });
+    }
 }
