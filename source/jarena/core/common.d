28de5bc326bfb66b3c82318836bb1e9f82425b89
SealabJaster <sealabjaster@gmail.com>
2018-03-21 18:18:33 +0000

    Make InitInfo's code generation a bit more sane

--- source/jarena/core/common.d
+++ source/jarena/core/common.d
@@ -1,6 +1,11 @@
 /// Contains common code/data structures that don't properly fit into any existing module
 module jarena.core.common;
 
+private struct InitProperty
+{
+    string publicName;
+}
+
 /++
  + A static class containing useful information some classes may need while initialising, but would be cumbersome to actually
  + pass through properly.
@@ -18,35 +23,42 @@ class InitInfo
         // What I really care about is being able to do "_windowSize in _locks" as an example.
         bool[string] _locks;
 
+        @InitProperty("windowSize")
         uvec2 _windowSize;
     }
 
     public static
     {
-        mixin(generateProperties("_windowSize", "uvec2", "windowSize"));
+        mixin(generateProperties!_windowSize);
     }
 
-    private static dstring generateProperties(string varName, string typeName, string propertyName)
+    private static dstring generateProperties(alias Var)()
     {
+        import std.traits : hasUDA, getUDAs;
         import codebuilder;
 
+        static assert(hasUDA!(Var, InitProperty), "The variable '" ~ Var.stringof ~ "' needs an @InitProperty");        
         auto builder = new CodeBuilder();
 
+        enum PropertyName   = getUDAs!(Var, InitProperty)[0].publicName;
+        enum TypeName       = typeof(Var).stringof;
+        enum VarName        = Var.stringof; 
+        
         // Setter
-        builder.putf("void %s(%s value) @safe nothrow", propertyName, typeName);
+        builder.putf("void %s(%s value) @safe nothrow", PropertyName, TypeName);
         builder.putScope((_)
         {
-            builder.putf("assert((\"%s\" in _locks) is null, \"The value for '%s' has already been set!\");", varName, varName);
-            builder.putf("_locks[\"%s\"] = false;", varName);
-            builder.putf("%s = value;", varName);
+            builder.putf("assert((\"%s\" in _locks) is null, \"The value for '%s' has already been set!\");", VarName, VarName);
+            builder.putf("_locks[\"%s\"] = false;", VarName);
+            builder.putf("%s = value;", VarName);
         });
 
         // Getter
-        builder.putf("const(%s) %s() @safe @nogc nothrow", typeName, propertyName);
+        builder.putf("const(%s) %s() @safe @nogc nothrow", TypeName, PropertyName);
         builder.putScope((_)
         {
-            builder.putf("assert((\"%s\" in _locks) !is null, \"The value for '%s' hasn't been set yet!\");", varName, varName);
-            builder.putf("return %s;", varName);
+            builder.putf("assert((\"%s\" in _locks) !is null, \"The value for '%s' hasn't been set yet!\");", VarName, VarName);
+            builder.putf("return %s;", VarName);
         });
 
         return builder.data.idup;
