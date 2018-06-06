f70aa831675ce1e82ea6f987b011d08bc186fa40
SealabJaster <sealabjaster@gmail.com>
2018-06-06 14:27:14 +0100

    Misc (Formatting)

--- source/jarena/gameplay/engine.d
+++ source/jarena/gameplay/engine.d
@@ -7,17 +7,17 @@ private
     import jarena.audio, jarena.core, jarena.graphics, jarena.gameplay, jarena.data;
     import opengl;
 
-    const ENGINE_CONFIG_PATH        = "data/engineConf.sdl";
-    const ENGINE_DATA_PATH          = "data/data.sdl";
-    const WINDOW_NAME               = "JArena";
-    const WINDOW_DEFAULT_SIZE       = uvec2(860, 740);
-    const WINDOW_DEFAULT_FPS        = 60;
-    const WINDOW_DEFAULT_VSYNC      = true;
-    const DEBUG_FONT                = "Data/Fonts/Spaceport_2006.otf";
-    const DEBUG_FONT_SIZE           = 10;
-    const DEBUG_TEXT_COLOUR         = Colours.rockSalt;
-    const DEBUG_CONTAINER_COLOUR    = Colour(0, 0, 0, 128);
-    const DEBUG_CONTAINER_POSITION  = vec2(1);
+    const ENGINE_CONFIG_PATH       = "data/engineConf.sdl";
+    const ENGINE_DATA_PATH         = "data/data.sdl";
+    const WINDOW_NAME              = "JArena";
+    const WINDOW_DEFAULT_SIZE      = uvec2(860, 740);
+    const WINDOW_DEFAULT_FPS       = 60;
+    const WINDOW_DEFAULT_VSYNC     = true;
+    const DEBUG_FONT               = "Data/Fonts/Spaceport_2006.otf";
+    const DEBUG_FONT_SIZE          = 10;
+    const DEBUG_TEXT_COLOUR        = Colours.rockSalt;
+    const DEBUG_CONTAINER_COLOUR   = Colour(0, 0, 0, 128);
+    const DEBUG_CONTAINER_POSITION = vec2(1);
 }
 
 /++
